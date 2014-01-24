52b1f08cfb91796bc9c694aa1def6228145eca62
tamer1an <trybynenko@gmail.com>
2014-01-24 15:15:45 +0200

    js app

---
+++ js/print.js
@@ -0,0 +1,20 @@
+﻿
+$(function() {
+  if (window.onbeforeprint !== undefined) {  
+      window.onbeforeprint = ShowLinks;
+      window.onafterprint = HideLinks;
+  }
+});
+
+function ShowLinks() {  
+  $("a").each(function() {
+    $(this).data("linkText", $(this).text());
+    $(this).append(" (" + $(this).attr("href") + ")");                
+  });
+}
+
+function HideLinks() {   
+  $("a").each(function() {
+    $(this).text($(this).data("linkText"));
+  });
+}
