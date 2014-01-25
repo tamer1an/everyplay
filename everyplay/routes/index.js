22fe89c5b26448d4956716438443e5b2adb2b0ee
tamer1an <trybynenko@gmail.com>
2014-01-25 18:08:04 +0200

    Backbone App blueprint

--- everyplay/routes/index.js
+++ everyplay/routes/index.js
@@ -1,8 +1,23 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express', scripts: ['javascripts/app.js'], css: ['stylesheets/reset.css','stylesheets/style.css']} ); //'stylesheets/print.css'
+exports.index = function (req, res) {
+    res.render('index', {
+        title: 'Express',
+
+        //JQuery Google CDN
+        //ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" // 2.0.3
+        //ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js
+
+        scripts: [
+            '//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.js',
+            'javascripts/lib/underscore.js',
+            'javascripts/lib/backbone.js',
+            'javascripts/app.js'
+        ]
+
+        //production
+        //scripts: ['//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js','javascripts/lib/underscore.min.js','javascripts/lib/backbone.min.js','javascripts/app.js']
+    });
 };
