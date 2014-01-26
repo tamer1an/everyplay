56a25316d87ff837e02ef009e11feff15434a9d6
tamer1an <trybynenko@gmail.com>
2014-01-26 04:04:33 +0200

    remote data video-list added & style refined

--- everyplay/public/javascripts/app.js
+++ everyplay/public/javascripts/app.js
@@ -1,13 +1,58 @@
 var Application = (function ($) {
     var App = {
         stores : {},
-        views : {}
+        views : {},
+        presenters : {}
     };
 
+    //VIDEO LIST PAGE VIEW
+    var videoListPage = Backbone.View.extend({
+        el : '#videos',
+        selectors:{
+            videos_thumb:'section[data-role="content"] div > img'
+        },
+        events : {
+            'click button#add': 'addItem'
+        },
+        initialize : function () {
+            _.bindAll(this, 'render','addItem','appendItem');
+
+            this.collection = App.stores.videoList;
+            this.collection.bind('add', this.appendItem);
+
+            this.render();
+        },
+        render : function () {
+            var self = this;
+            $(this.el).append("<button id='add'>Add list item</button>");
+            $(this.el).append("<ul></ul>");
+            _(this.collection.models).each(function(item){ // in case collection is not empty
+                self.appendItem(item);
+            }, this);
+        },
+        //Model Event Handlers
+        addItem: function(){
+            var item = new videoItem();
+            item.set({
+                part2: item.get('part2') // modify item defaults
+            });
+            this.collection.add(item); // add item to collection; view is updated via event 'add'
+        },
+        appendItem: function(model){
+            var itemView = new ItemView({
+                model: model
+            });
+            $('ul', this.el).append(itemView.render().el);
+        }
+    });
+
+
+//// MODEL & COLLECTIONS
     var videoItem = Backbone.Model.extend({
+//        url:'https://everyplay.com/api/videos',
         defaults:function(){
 
-          return  { // video Entity
+            return  { // video Entity
                 part1: 'hello',
                 part2: 'world',
 
@@ -51,8 +96,53 @@ var Application = (function ($) {
         }
     });
 
-    var videoList = Backbone.Collection.extend({
-        model: videoItem
+    var videoListStore = Backbone.Collection.extend({
+        url:'https://everyplay.com/api/videos?',
+        initialize: function(){
+            this.dropdown = $('#video-filter');
+            this.url += this.createGETString();
+        },
+        model: videoItem,
+
+        URL: 'https://everyplay.com/api/videos?',
+        client_id:'336d586b6e1b5e4a0f9eaa48e7e697d8cd51db40',
+        defaultVideosQuery:{
+            offset:0,
+            limit:24
+        },
+        lastVideosQuery:{ },
+        lastVideosQueryResult:[],
+
+        createGETString:function(){
+            var options = this.getDefaultVideosQuery();
+            options.client_id = this.getClientId();
+            options.order = this.dropdown.val();
+
+            var requestString = '';
+            for (var item in options){
+                requestString += [item,'=',options[item],'&'].join("")
+            }
+            return requestString;
+        },
+
+        //list history
+        getDefaultVideosQuery:function(){
+            return this.defaultVideosQuery;
+        },
+        setLastVideosQueryResult:function(arr){
+            this.lastVideosQueryResult=arr;
+        },
+        setLastVideosQuery:function(options){
+            this.lastVideosQuery=options;
+        },
+
+        //Prop Getters
+        getClientId:function(){
+            return this.client_id;
+        },
+        getURL:function(options){
+            return this.URL+options;
+        }
     });
 
     var ItemView = Backbone.View.extend({
@@ -78,94 +168,40 @@ var Application = (function ($) {
         }
     });
 
-    //VIDEO LIST PAGE
-    var videoListPage = Backbone.View.extend({
-        client_id:'336d586b6e1b5e4a0f9eaa48e7e697d8cd51db40',
-        defaultVideosQuery:{
-            offset:0,
-            limit:24
-        },
-        lastVideosQuery:{ },
-        lastVideosQueryResult:[],
-        selectors:{
-            videos_thumb:'section[data-role="content"] div > img'
-        },
-        URL: 'https://everyplay.com/api/videos?',
-        el : '#videos',
-        events : {
-            "change #video-filter" : "updateVideolist",
-            'click button#add': 'addItem'
-        },
-        initialize : function () {
-            _.bindAll(this, 'updateVideolist', 'render'      ,'addItem', 'appendItem');
-
-            this.collection = new videoList();
-            this.collection.bind('add', this.appendItem); // collection event binder
-
-            this.render();
-        },
-        render : function () {
-            var self = this;
-            $(this.el).append("<button id='add'>Add list item</button>");
-            $(this.el).append("<ul></ul>");
-            _(this.collection.models).each(function(item){ // in case collection is not empty
-                self.appendItem(item);
-            }, this);
-        },
-        updateVideolist : function () {
-            alert(1)
-        },
-
-
-        //list history
-        getDefaultVideosQuery:function(){
-            return this.defaultVideosQuery;
-        },
-        setLastVideosQueryResult:function(arr){
-            this.lastVideosQueryResult=arr;
-        },
-        setLastVideosQuery:function(options){
-            this.lastVideosQuery=options;
-        },
-
-        //Prop Getters
-        getClientId:function(){
-            return this.client_id;
-        },
-        getURL:function(options){
-            return this.URL+options;
-        },
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-        addItem: function(){
-            var item = new videoItem();
-            item.set({
-                part2: item.get('part2') // modify item defaults
-            });
-            this.collection.add(item); // add item to collection; view is updated via event 'add'
-        },
-        appendItem: function(item){
-            var itemView = new ItemView({
-                model: item
-            });
-            $('ul', this.el).append(itemView.render().el);
-        }
-    });
-
-
     //Single Video Page
     var videoSinglePage = Backbone.View.extend({
-        el : 'body',
+        el : 'footer',
         events : {
             // "load" :function(e) { alert(e.type); },
         },
         initialize : function () {
-            _.bindAll(this, 'render');
+            _.bindAll(this,'render');
             this.render();
         },
         render : function () {
+        }
+    });
 
+    //AppControler
+    var AppControler = Backbone.View.extend({
+        el : 'body',
+        events : {
+            "change #video-filter" : "updateVideolist"
+        },
+        initialize : function () {
+            _.bindAll(this,'updateVideolist', 'render');
+           this.videoListStore  = App.stores.videoList = new videoListStore();
+           this.videoListPage   = App.views.videoList  = new videoListPage;
+
+           this.videoListStore.fetch();
+
+           this.render();
+        },
+        render : function () {
+
+        },
+        updateVideolist : function () {
+            alert(1)
         }
     });
 
@@ -175,8 +211,8 @@ var Application = (function ($) {
             e.preventDefault();
         };
 
-        // App.views.videoSinglePage = new videoSinglePage;
-        App.views.videoPage       = new videoListPage;
+        App.presenters.AppControler = new AppControler;
+
     });
 
     return App;
